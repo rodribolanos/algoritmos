0037feae18fa04ed55c965d23f2ca779b4d44af1
rodrigo.bolanos <rodrimanuelbw@gmail.com>
2026-04-11 19:56:08 -0300

    agrego programas base sobre tipos de inputs

---
+++ trucos/infiniteInput.cpp
@@ -0,0 +1,11 @@
+// programa sobre como pedir un input "infinito" (corta cuando el usuario le da Ctrl C), no tiene cantidad de entrada
+#include <iostream>
+
+int main() {
+    int entrada;
+    while (cin >> entrada) {
+        
+        cout << hacerAlgo() << "\n"
+        
+    }
+}
---
+++ trucos/testCases.cpp
@@ -0,0 +1,13 @@
+// aca va un pequenio programa que espera N test cases y por cada uno hace algo
+#include <iostream>
+
+
+int main() {
+    int testCases;
+
+    while(testCases--) {
+        cout << hacerAlgo();
+        if testCases != 1 cout << "\n"
+
+    }
+}
