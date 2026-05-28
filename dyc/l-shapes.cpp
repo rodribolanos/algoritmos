09facb8cb53150d0d4364e5b8f2771c53e8ab055
rodrigo.bolanos <rodrimanuelbw@gmail.com>
2026-05-28 19:10:04 -0300

    add l shapes finished

--- dyc/l-shapes.cpp
+++ dyc/l-shapes.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 vector<vector<char>> matrix;
 
+char currentLetter = 'A';
 
-long posicionDeLAColocar(long fromX, long toX, long fromY, long toY, long x, long y, long mid) {
-    if (fromX <= x && x < mid) {
-        if (fromY <= y && y < mid) {
+long posicionDeLAColocar(long fromX, long toX, long fromY, long toY, long x, long y, long midX, long midY) {
+    if (fromX <= x && x < midX) {
+        if (fromY <= y && y < midY) {
             // esta en el cuadrante izquierdo de X y superior de Y
             return 1;
         } else {
@@ -18,7 +19,7 @@ long posicionDeLAColocar(long fromX, long toX, long fromY, long toY, long x, lon
         }
     } else {
         // esta en cuadrante derecho de x
-        if (fromY <= y && y < mid) {
+        if (fromY <= y && y < midY) {
             return 2;
             // esta en el cuadrante derecho de X y superior de Y
         } else {
@@ -32,78 +33,106 @@ long posicionDeLAColocar(long fromX, long toX, long fromY, long toY, long x, lon
 
 // (x,y) representa la posicion exacta donde hay "un punto", o en este caso un espacio completo
 void fill(long fromX, long toX, long fromY, long toY, long x, long y) {
+    // como es cuadrada puedo usar cualquiera de los dos
+    if (toX - fromX == 1) return;
 
-    if (toX - fromX == 1 && toY - fromY == 1) return;
-    // es lo mismo usar X que Y, porque es una matriz cuadrada
-    long mid = (fromX + toX) / 2;
+    long midX = (fromX + toX) / 2;
     
-    long lPosition = posicionDeLAColocar(fromX, toX, fromY, toY, x, y, mid);
+    long midY = (fromY + toY) / 2;
+    long lPosition = posicionDeLAColocar(fromX, toX, fromY, toY, x, y, midX, midY);
 
+    char myLetter = currentLetter;
+    currentLetter++;
+    if (currentLetter > 'Z') {
+        currentLetter = 'A'; // Volvemos a empezar si nos pasamos de la Z
+    
+    }
     if (lPosition == 1) {
         // no tengo que poner en la matriz superior izquierda
-        fill(fromX, mid, fromY, mid, x, y);
+        fill(fromX, midX, fromY, midY, x, y);
+        
         // matriz inferior izquierda, misma X, desplazo 1 hacia arriba
-        matrix[mid - 1][mid] = 'A';
-        fill(fromX, mid, mid, toY, mid - 1, mid);
+        matrix[midX - 1][midY] = myLetter;
+        fill(fromX, midX, midY, toY, midX - 1, midY);
+        
         // matriz superior derecha, misma altura, voy 1 a la derecha 
-        matrix[mid][mid - 1] = 'A'; 
-        fill(mid, toX, fromY, mid, mid, mid - 1);
+        matrix[midX][midY - 1] = myLetter; 
+        fill(midX, toX, fromY, midY, midX, midY - 1);
+        
         // matriz inferior derecha, me muevo 1 a la derecha, y bajo 1
-        matrix[mid][mid] = 'A';
-        fill(mid, toX, mid, toY, mid, mid);
+        matrix[midX][midY] = myLetter;
+        fill(midX, toX, midY, toY, midX, midY);
 
     } else if (lPosition == 2) {
         // no tengo que poner en la matriz superior derecha
-        fill(mid, toX, mid, toY, x, y);
+        fill(midX, toX, fromY, midY, x, y);
+        
         // matriz superior izquierda, no me muevo
-        matrix[mid - 1][mid - 1] = 'B';
-        fill(fromX, mid, fromY, mid, mid - 1, mid - 1);
+        matrix[midX - 1][midY - 1] = myLetter;
+        fill(fromX, midX, fromY, midY, midX - 1, midY - 1);
+        
         // matriz inferior izquierda, mismo X, desplazo 1 hacia abajo
-        matrix[mid - 1][mid] = 'B';
-        fill(mid, toX, fromY, mid, mid, mid - 1);
+        matrix[midX - 1][midY] = myLetter;
+        fill(fromX, midX, midY, toY, midX - 1, midY);
+        
         // matriz inferior derecha, desplazo en diagonal
-        matrix[mid][mid] = 'B';
-        fill(mid, toX, mid, toY, mid, mid);
+        matrix[midX][midY] = myLetter;
+        fill(midX, toX, midY, toY, midX, midY);
+
     } else if (lPosition == 3) {
         // no tengo que poner . en la matriz inferior derecha
-        fill(mid, toX, mid, toY, x, y);
+        fill(midX, toX, midY, toY, x, y);
+        
         // matriz superior izquierda
-        matrix[mid - 1][mid - 1] = 'C';
-        fill(fromX, mid, fromY, mid, mid - 1, mid - 1);
+        matrix[midX - 1][midY - 1] = myLetter;
+        fill(fromX, midX, fromY, midY, midX - 1, midY - 1);
+        
         // matriz superior derecha, me muevo 1 en X 
-        matrix[mid][mid - 1] = 'C';
-        fill(mid, toX, fromY, mid, mid, mid - 1);
+        matrix[midX][midY - 1] = myLetter;
+        fill(midX, toX, fromY, midY, midX, midY - 1);
+        
         // matriz inferior izquierda, me quedo en X, bajo en Y
-        matrix[mid - 1][mid] = 'C';
-        fill(mid, toX, mid, toY, mid - 1, mid);
+        matrix[midX - 1][midY] = myLetter;
+        fill(fromX, midX, midY, toY, midX - 1, midY);
     } else {
         // en este caso, el punto esta en en la matriz abajo a la izquierda
-        fill(fromX, mid, mid, toY, x, y);
+        fill(fromX, midX, midY, toY, x, y);
+        
         // matriz superior izquierda
-        matrix[mid - 1][mid - 1] = 'D';
-        fill(fromX, mid, fromY, mid, mid - 1, mid - 1);
+        matrix[midX - 1][midY - 1] = myLetter;
+        fill(fromX, midX, fromY, midY, midX - 1, midY - 1);
+        
         // matriz superior derecha 
-        matrix[mid][mid - 1] = 'D';
-        fill(mid, toX, fromY, mid, mid, mid - 1);
+        matrix[midX][midY - 1] = myLetter;
+        fill(midX, toX, fromY, midY, midX, midY - 1);
+        
         // matriz inferior derecha
-        matrix[mid][mid] = 'D';
-        fill(mid, toX, mid, toY, mid, mid);
+        matrix[midX][midY] = myLetter;
+        fill(midX, toX, midY, toY, midX, midY);
     }
 }
 
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
     long n; cin >> n;
 
     long x, y; cin >> x >> y;
+
     matrix.resize(n+1, vector<char>(n+1));
 
     matrix[x][y] = '.';
-
     fill(1, n + 1, 1, n + 1, x, y);
 
-    for(int f = 1; f < n + 1; f++) {
-        for (int c = 1; c < n + 1; c++)
-        cout << matrix[f][c];
+    for(int f = 1; f <= n; f++) {
+        for (int c = 1; c <= n; c++) {
+            cout << matrix[f][c];
+        }
+        cout << "\n";
     }
+
+    return 0;
 }
